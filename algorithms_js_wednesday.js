0d944380f473385bf68ce9c6459de4a21e2464cb
kaviya6592 <120781364+kaviya6592@users.noreply.github.com>
2025-06-04 22:57:35 +0200

    Add files via upload

---
+++ algorithms_js_wednesday.js
@@ -0,0 +1,13 @@
+//Wednesday: Factorialize!
+//Declare a function factorialize(n) that takes an integer n as input and returns the factorial of the provided integer.
+
+//If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+
+//Factorials are often represented with the shorthand notation n!
+
+//For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+let fact=1;
+for(i=1;i<10;i++){
+    fact=fact * (i+1);
+}console.log(fact);
+
