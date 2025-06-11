3836065c4ea919f5607885eb9259d3903d33eabf
kaviya6592 <120781364+kaviya6592@users.noreply.github.com>
2025-06-11 15:42:16 +0200

    Add files via upload

---
+++ thursday_1.js
@@ -0,0 +1,25 @@
+//Thursday: Sum All Numbers in a Range
+//We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
+
+
+function sumAll(arr) {
+    let sum=0;
+    for(i=Math.min(...arr);i<Math.max(...arr);i++){
+        sum +=i;
+    }
+    return sum
+        
+}     
+
+        //if(arr[i]<arr[i+1]){
+            
+         //   let incerement=arr[i]+1;
+            //arr1.push(incerement)
+        //}
+  
+
+console.log(sumAll([5, 10]));
+
+
+
+
